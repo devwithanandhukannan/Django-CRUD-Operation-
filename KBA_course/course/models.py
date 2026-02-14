669a3277b2a0d3adebdd8cefce73df6527a9a1b3
Anandhu <connect.anandhukannan@gmail.com>
2026-02-14 17:09:30 +0530

    added [COURSE model]

--- KBA_course/course/models.py
+++ KBA_course/course/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return self.name
+    
+class Course(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.DecimalField()
+    category = models.ForeignKey(
+        Category,
+        on_delete = models.CASCADE,
+        related_name='course'
+    )
+    def __str__(self):
+        return self.name
